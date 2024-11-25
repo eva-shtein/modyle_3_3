e6e92e34a07ba8ce1afe69e6be558e302c6fca22
eva-shtein <eva.a.shtein@gmail.com>
2024-11-25 12:21:27 +1000

    Add files via upload

---
+++ modyle_3_3.py
@@ -0,0 +1,16 @@
+#def print_params(a = 1, b = "строка", c = True):
+    #print(a, b, c)
+#print_params()
+#print_params(b = 25)
+#print_params(c = [1, 2, 3])
+
+def print_params(*args, **kwargs):
+    print(args, kwargs)
+
+values_list = [3, "Urban", False]
+values_dict = {'a': 5, 'b': "строка", 'c': True}
+
+print_params(*values_list, **values_dict)
+
+values_list_2 = [12, 'lost']
+print_params(*values_list_2, 42)
