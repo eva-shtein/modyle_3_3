da57e01820836bbbf272d7f30f3ab7de3ce3e719
eva-shtein <eva.a.shtein@gmail.com>
2025-01-15 21:42:27 +1000

    Update modyle_3_3.py

--- modyle_3_3.py
+++ modyle_3_3.py
@@ -1,16 +1,14 @@
-#def print_params(a = 1, b = "строка", c = True):
-    #print(a, b, c)
-#print_params()
-#print_params(b = 25)
-#print_params(c = [1, 2, 3])
-
-def print_params(*args, **kwargs):
-    print(args, kwargs)
+def print_params(a = 1, b = "строка", c = True):
+    print(a, b, c)
+print_params()
+print_params(b = 25)
+print_params(c = [1, 2, 3])
 
 values_list = [3, "Urban", False]
 values_dict = {'a': 5, 'b': "строка", 'c': True}
 
-print_params(*values_list, **values_dict)
+print_params(*values_list)
+print_params(**values_dict)
 
 values_list_2 = [12, 'lost']
 print_params(*values_list_2, 42)
